f52e6fe0842cde40a91f0513b69a89c6447c5a8a
BlankHAX <stockfrellx@gmail.com>
2025-03-03 11:17:42 +0800

    Config.js

--- Config.js
+++ Config.js
@@ -1,12 +1,12 @@
 module.exports = {
     // your server name
-    server_name: "GrowPlus",
+    server_name: "UnityPS",
     // public ipv4
-    server_ip: "127.0.0.1",
+    server_ip: "178.128.89.174",
     // UDP port
     server_port: 17091,
     // login url dashboard
-    loginurl: "login.cdngtps.my.id",
+    loginurl: "unityps.my.id",
     // meta
     meta: "name=GrowPlus&ip=127.0.0.1&port=17091&3rd=0",
 }
